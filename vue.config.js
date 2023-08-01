530662e0baf091fc2e0a0e60d9d1cf51be3c096c
BARM <284942955@qq.com>
2023-08-01 13:50:38 +0000

    gitpod test

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,11 @@ const circularDependencyPlugin = require('circular-dependency-plugin')
 const isLib = process.env.TYPE === 'lib'
 
 module.exports = {
+  devServer:{
+    host: '0.0.0.0', 
+    disableHostCheck: true, 
+    port:8081
+  },
   chainWebpack (config) {
     if (!isLib) {
       config.plugin('monaco').use(new MonacoWebpackPlugin())
